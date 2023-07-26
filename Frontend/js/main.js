37e96f98342c964711999e69b6b43140e0e116e8
PieroZev <pzevallose94@gmail.com>
2023-07-25 21:59:44 -0500

    cambios de diseño

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -13,11 +13,15 @@ function fillDropdownCapital() {
             dataType: "JSON"
         }).done(function(data) {
             var text = "";
+            var capitals = new Array();
             text += "<option value='0'>Seleccione la capital</option>";
             $.each(data, function(index, value) {
-                if(value.capital)
-                text += "<option value='" + value.capital + "'>" + value.capital + "</option>";
+                if (value.capital && value.capital.length > 0) capitals.push(value.capital);
             });
+            capitals.sort();
+            for(let i = 0; i < capitals.length;i++){
+                    text += "<option value='" + capitals[i] + "'>" + capitals[i] + "</option>";
+            }
             $("#searchByCapital").html(text);
         }).fail(function(msg) {
             console.table(msg);
@@ -31,11 +35,15 @@ function fillDropdownCountry() {
         dataType: "JSON"
     }).done(function(data) {
         var text = "";
+        var countries = new Array();
         text += "<option value='0'>Seleccione el país</option>";
         $.each(data, function(index, value) {
-            if(value.name.common)
-            text += "<option value='" + value.name.common + "'>" + value.name.common + "</option>";
+            if(value.name.common) countries.push(value.name.common);
         });
+        countries.sort();
+            for(let i = 0; i < countries.length;i++){
+                    text += "<option value='" + countries[i] + "'>" + countries[i] + "</option>";
+            }
         $("#searchByCountry").html(text);
     }).fail(function(msg) {
         console.table(msg);
@@ -49,11 +57,15 @@ function fillDropdownContinent() {
         dataType: "JSON"
     }).done(function(data) {
         var text = "";
+        var continents = new Array();
         text += "<option value='0'>Seleccione el continente</option>";
         $.each(data, function(index, value) {
-            if(!text.includes(value.region))
-            text += "<option value='" + value.region + "'>" + value.region + "</option>";
+            if(!continents.includes(value.region)) continents.push(value.region);
         });
+        continents.sort();
+        for(let i = 0; i < continents.length;i++){
+                text += "<option value='" + continents[i] + "'>" + continents[i] + "</option>";
+        }
         $("#searchByContinent").html(text);
     }).fail(function(msg) {
         console.table(msg);
@@ -91,7 +103,7 @@ function showCountryDetailsList(comparisonString,selectedParam){
         method: "GET",
         dataType: "JSON"
     }).done(function(data) {
-        var text = "";
+        var text = "<h2>Resultados:</h2>";
         text += '<div class="row" id="labels"><div class="col-md-2">Pais</div><div class="col-md-3">Capital</div><div class="col-md-2">Continente</div><div class="col-md-2">Subcontinente</div><div class="col-md-3">Googlemaps Link</div></div>';
         $.each(data, function(index, value) {
             switch (comparisonString) {
@@ -101,12 +113,13 @@ function showCountryDetailsList(comparisonString,selectedParam){
                 default: break;
             }
             if(value && comparisonParam.includes(selectedParam)){
+            text += '<h4>Detalle de '+value.name.common+'</h4>';
             text += '<div class="row country-details">';
             text += '<div class="col-md-2">'+value.name.common+'</div>'//nombre
             text += '<div class="col-md-3">'+value.capital+'</div>'//capital
             text += '<div class="col-md-2">'+value.region+'</div>'//continente
             text += '<div class="col-md-2">'+value.subregion+'</div>'//subcontinente
-            text += '<div class="col-md-3"><a href="'+value.maps.googleMaps+'">Maps</a></div>'//googlemaps
+            text += '<div class="col-md-3 map"><a href="'+value.maps.googleMaps+'">Maps</a></div>'//googlemaps
             text += '</div>'
             }
         });
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Prueba-Optima-Inmobiliaria-
+
+Se agregó mejoras en el diseño:
+-Utilicé el logo de la empresa y los colores.
+-Dispuse los listados en orden alfabético.
+-Se dispuso el listado de continentes primero, luego el de países
+y al final el de capitales, para dar un mejor sentido al usuario.
+-Se utilizaron buenas prácticas y se intento no redundar en código.
+-En general, una mejor disposición de UI/UX para hacer la página más
+amigable al usuario.
